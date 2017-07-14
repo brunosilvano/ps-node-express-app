d4fcd5b0f84bfb06bb61f2088815805ecf1171d9
Bruno Silvano <bruno_705@hotmail.com>
2017-07-14 13:22:29 -0300

    simple routing

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var express = require('express');
 var app = express();
 
 var port = 5000;
-app.listen(port, function(err) {
+
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
+app.get('/books', function(req, res) {
+    res.send('Hello Books');
+});
+
+app.listen(port, function (err) {
     console.log('running server on port: ' + port);
 });
