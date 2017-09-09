9079cc730259439d13ff206eb67e43983551cda2
Bruno Silvano <bruno_705@hotmail.com>
2017-09-09 01:14:38 -0300

    added routing using Router

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+    .get(function (req, res) {
+        res.send('Hello Books');
+    });
+
+    bookRouter.route('/single')
+    .get(function (req, res) {
+        res.send('Hello Single Books');
+    });
+
+app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
