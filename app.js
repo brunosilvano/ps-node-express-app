063c1a9301a1914fe892b41b6a88980f2466a52c
Bruno Silvano <bruno_705@hotmail.com>
2017-09-09 01:26:29 -0300

    books rendering

--- app.js
+++ app.js
@@ -10,12 +10,34 @@ app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+var books = [{
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich',
+    read: false
+}, {
+    title: 'Les Misérables',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich',
+    read: false
+}];
+
 bookRouter.route('/')
     .get(function (req, res) {
-        res.send('Hello Books');
+        res.render('books', {
+            title: 'Books',
+            nav: [{
+                Link: '/Books',
+                Text: 'Books'
+            }, {
+                Link: '/Authors',
+                Text: 'Authors'
+            }],
+            books: books
+        });
     });
 
-    bookRouter.route('/single')
+bookRouter.route('/single')
     .get(function (req, res) {
         res.send('Hello Single Books');
     });
