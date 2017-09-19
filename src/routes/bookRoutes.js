49edcb80399b28cb90ea35a28a790b9d272f8ff4
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-09-18 23:25:43 -0300

    find one database entry

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,37 +1,41 @@
 var express = require('express');
-
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var bookRouter = express.Router();
 
 var router = function (nav) {
-    var books = [{
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich',
-        read: false
-    }, {
-        title: 'Les Misérables',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich',
-        read: false
-    }];
 
     bookRouter.route('/')
         .get(function (req, res) {
-            res.render('bookListView', {
-                title: 'Books',
-                nav: nav,
-                books: books
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.find({}).toArray(function (err, results) {
+                    res.render('bookListView', {
+                        title: 'Books',
+                        nav: nav,
+                        books: results
+                    });
+                });
             });
         });
 
     bookRouter.route('/:id')
         .get(function (req, res) {
-            var id = req.params.id;
-            res.render('bookView', {
-                title: 'Books',
-                nav: nav,
-                book: books[id]
+            var id = new objectId(req.params.id);
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.findOne({ _id: id }, function (err, result) {
+                    res.render('bookView', {
+                        title: 'Books',
+                        nav: nav,
+                        book: result
+                    });
+                });
             });
+
+
         });
 
     return bookRouter;
