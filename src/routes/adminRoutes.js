217e1238529c4e71409921c27840433993b9f11a
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-09-18 23:00:42 -0300

    books insertion in DB

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+var books = [{
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich',
+    read: false
+}, {
+    title: 'Les Misérables',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich',
+    read: false
+}];
 
 var router = function (nav) {
     adminRouter.route('/addBooks')
         .get(function (req, res) {
-            res.send('inserting books');
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.insertMany(books, function (err, results) {
+                    res.send(results);
+                    db.close();
+                });
+            });
         });
 
     return adminRouter;
