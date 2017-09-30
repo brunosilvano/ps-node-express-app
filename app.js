ea70ca64950ab1ff61e97c3d92d539a27d869bde
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-09-30 02:36:32 -0300

    sign-up using local strategy

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var app = express();
 
@@ -20,6 +22,9 @@ var authRouter = require('./src/routes/authRoutes')(nav);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+require('./src/config/passport')(app);
 
 app.set('views', './src/views');
 
